4621f30459cd0568767d0b1c19e4f9a82e2d0ff9
HaidaraIB <nezarmaarof@gmail.com>
2025-09-20 02:02:38 +0300

    solve django.core.exceptions.ImproperlyConfigured: You're using the staticfiles app without having set the STATIC_ROOT setting to a filesystem path.

--- core/settings.py
+++ core/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
