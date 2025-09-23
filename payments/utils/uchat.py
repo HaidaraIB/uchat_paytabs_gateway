deed23ae818e4d0f905cf30e615354d9d85f8c72
HaidaraIB <nezarmaarof@gmail.com>
2025-09-23 20:58:15 +0000

    enable auto renew and more

--- payments/utils/uchat.py
+++ payments/utils/uchat.py
@@ -45,13 +45,25 @@ def change_plan(workspace_id: int, owner_email: str, plan_id: str):
     )
     change_plan_res = safe_json(response)
     logger.info("Change Plan Response: %s", change_plan_res)
+
     if change_plan_res.get("status", None) == "ok":
+
+        # Auto renew
+        response = requests.post(
+            url=f"{settings.UCHAT_BASE_URL}/workspace/{workspace_id}/enable-auto-renew",
+            headers={
+                "authorization": f"Bearer {settings.UCHAT_TOKEN}",
+            },
+        )
+        auto_renew_res = safe_json(response)
+        logger.info("Auto Renew Response: %s", auto_renew_res)
+
         return workspace_id
     return False
 
 
 def safe_json(response: requests.Response):
-    """Try to parse JSON, return None if empty or invalid."""
+    """Try to parse JSON, return an empty dict if empty or invalid."""
     try:
         return response.json()
     except ValueError:
